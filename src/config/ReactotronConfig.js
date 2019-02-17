1c3f0f6e2f275a460af7a6cf97b6f5d18b22be0e
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-02-17 13:46:07 -0300

    Update ReactotronConfig.js

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -2,9 +2,11 @@ import { NativeModules } from 'react-native';
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  const scriptURL = NativeModules.SourceCode.scriptURL;
-  scriptHostname = scriptURL.split('://')[1].split(':')[0];
-  const tron = Reactotron.configure({ host: scriptHostname }).connect();
+  const { scriptURL } = NativeModules.SourceCode;
+  const scriptHostname = scriptURL.split('://')[1].split(':')[0];
+  const tron = Reactotron.configure({ host: scriptHostname })
+    .useReactNative()
+    .connect();
 
   tron.clear();
 
